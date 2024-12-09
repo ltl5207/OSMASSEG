abe6d5be4f039f0682a27c1762a13f3a38657ae7
ltl5207 <43669126+ltl5207@users.noreply.github.com>
2024-12-10 01:46:53 +0800

    Update test_pddca.py

--- trans-unet/TransUNet/test_code/test_pddca.py
+++ trans-unet/TransUNet/test_code/test_pddca.py
@@ -22,7 +22,7 @@ from datasets.dataset_newsyn40_gan import newsyn40_dataset,RandomGenerator
 from datasets.dataset_syn40_chenxi import syn40_chenxi_dataset
 from datasets.dataset_pddca_syn36 import pddca_syn36_dataset
 # need to be modified to the dir of the translation model(GAN)
-sys.path.append('../../../triple/')
+sys.path.append('../../../translation/')
 import os
 from options.test_options import TestOptions
 from data import create_dataset
